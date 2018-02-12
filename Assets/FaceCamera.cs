d20717a2cd93df0b07c904ccaa94980b94d397a6
DESKTOP-U0S4GM5\John <jfpforster@gmail.com>
2018-02-12 18:01:04 +0000

    Initial Commit

---
+++ Assets/FaceCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour {
+
+	private CameraTarget cameraTarget;
+	private Transform targetTransform;
+	// Use this for initialization
+	void Start () {
+		cameraTarget = FindObjectOfType<CameraTarget> ();
+		targetTransform = cameraTarget.gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (targetTransform != null) {
+			transform.LookAt(targetTransform);
+		}
+	}
+}
---
+++ Assets/FollowPlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerScript : MonoBehaviour {
+
+	public GameObject target;
+	public float distance = 3.3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(target.transform.position.x*0.8f, transform.position.y, target.transform.position.z - distance);
+	}
+}
